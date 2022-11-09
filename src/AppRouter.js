656b37cb9ee3df6d641ed06f8fea0de687cc0906
tntis <tnbinsu@gmail.com>
2022-11-09 21:29:15 +0900

    70번째

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,25 +1,38 @@
-import React from "react";
-import { BrowserRouter, Route , Routes} from "react-router-dom";
+import React from 'react';
 import './index.css';
-import App from "./App";
-import Login from "./Login";
+import App from './App';
+import Login from './Login';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Box, Typography} from '@mui/material'; //추가
 
+class AppRouter extends React.Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+					{/* 추가 */}
+          <Box mt={5}>
+            <Copyright />
+          </Box>
+        </BrowserRouter>
+      </div>
+    )
+  }
+}
 
-class AppRouter extends React.Component{
-    render(){
-        return(
-            <div>
-                <BrowserRouter>
-                    <div>
-                        <Routes>
-                            <Route path="/" element={<App />}></Route>
-                            <Route path="/login" element={<Login />}></Route>
-                        </Routes>
-                    </div>
-                </BrowserRouter>
-            </div>
-        );
-    }
+//추가
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      Copyright © Java Jocture Study, {new Date().getFullYear()}.
+    </Typography>
+  )
 }
 
 export default AppRouter;
--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,11 +1,21 @@
 import {API_BASE_URL} from "../app-config.js";
 
+const ACCESS_TOKEN = 'ACCESS_TOKEN';
+
 export function call(api, method, data){
     const url = API_BASE_URL + api; 
+
+    let headers = new Headers({
+        "Content-Type" : "application/json"
+    });
+
+    const accesToken = localStorage.getItem(ACCESS_TOKEN);
+    if(accesToken !== null && accesToken){
+        headers.append('Authorization','Bearer ' + accesToken);
+    }
+
     let options = {
-        headers : new Headers(
-            {"Content-Type" : "application/json"}
-        ),
+        headers : headers,
         method : method
     }
 
@@ -34,8 +44,21 @@ export function call(api, method, data){
 
 
 export function callLogin(userLogin){
-    return call('/auth/login/v3', "POST", userLogin)
-    .then(response => {
-        console.log(response);
+    return call('/auth/v4/login', 'POST', userLogin)
+    .then(body => {
+        console.log(body);
+        if(body.result && body.result.data && body.result.data.token){
+            const accessToken = body.result.data.token;
+            console.warn(`JWT 토큰 : ${accessToken}`);
+            alert('JWT 토큰 : '+ accessToken);
+            
+            // 웹 스토리지
+            // - 세션 스토리지 : 브라우저를 닫으면 초기화(삭제)
+            // - 로컬 스토리지 : 브라우저를 닫아도 유지
+
+            localStorage.setItem(ACCESS_TOKEN, accessToken);
+
+            window.location.href="/";
+        }
     });
 }
